330135fed8d5009d2f66f0bb784db51d2f431871
Michał Nawrocki <m.nawrocki77@o2.pl>
2023-11-02 15:04:15 +0100

    commiting before trying to fix a bug where the Animation Riggin package sees a gun as a bone

--- Assets/Scripts/Player Scritps/PlayerController/AnimationsController.cs
+++ Assets/Scripts/Player Scritps/PlayerController/AnimationsController.cs
@@ -9,17 +9,17 @@ public class AnimationsController : MonoBehaviour
 {
 	Animator animator;
 	[SerializeField] Transform leftHandAnchor;
+	[SerializeField] Transform leftElbowAnchor;
 	private void Start()
 	{
 		animator = GetComponent<Animator>();
 	}
 	private void OnAnimatorIK(int layerIndex)
 	{
+		animator.SetIKHintPosition(AvatarIKHint.LeftElbow, leftElbowAnchor.position);
+		animator.SetIKHintPositionWeight(AvatarIKHint.LeftElbow, 1);
+
 		animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandAnchor.position);
 		animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
-
-		animator.SetIKRotation(AvatarIKGoal.RightHand, leftHandAnchor.rotation);
-		animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
-
 	}
 }
